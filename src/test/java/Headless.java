43204b7122ffb856da679ac218c54647e7c26b93
GURU-JAVA <54605558+beehive04@users.noreply.github.com>
2019-10-13 10:14:57 -0600

    1.st Commit

---
+++ src/test/java/Headless.java
@@ -0,0 +1,25 @@
+import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.concurrent.TimeUnit;
+
+public class Headless {
+    public static void main(String[] args) {
+
+        System.setProperty("webdriver.chrome.driver", "/Users/vahit.peker/Desktop/chromedriver");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless", "--disable-gpu", "--start-maximized", "--ignore-certificate-errors");
+        WebDriver driver = new ChromeDriver(options);
+        driver.get("http://www.facebook.com");
+
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        System.out.println("1");
+        driver.findElement(By.id("email")).sendKeys("headless");
+        System.out.println("done");
+
+
+    }
+}
---
+++ src/test/java/WebDriverManager.java
@@ -0,0 +1,25 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.opera.OperaDriver;
+import org.openqa.selenium.opera.OperaOptions;
+
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+public class WebDriverManager {
+    public static void main(String[] args) {
+        io.github.bonigarcia.wdm.WebDriverManager.operadriver().setup();
+       // System.setProperty("webdriver.opera.driver","/Users/vahit.peker/Desktop/operadriver");
+        //OperaOptions operaOptions=new OperaOptions();
+        //operaOptions.setBinary("/Users/vahit.peker/Desktop");
+        WebDriver driver=new OperaDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://www.seleniumhq.org");
+
+
+    }
+}
